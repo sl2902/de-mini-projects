8d97888ec5f0f73dacb4b971407dcf0f28fe7633
sl2902 <laxmsun@hotmail.com>
2024-02-16 20:55:30 +0530

    Add streamlit code

---
+++ airflow_pubsublite_bigquery/dags/config_parameters.py
@@ -0,0 +1,19 @@
+# gcp_info variables
+PROJECT_ID = "hive-413217"
+REGION = "us-central1",
+BUCKET_NAME = "mock-retail-data"
+BUCKET_OBJECT = "generated_data"
+DATASET = "retail"
+PRODUCT_TABLE = "product"
+STORE_TABLE = "store"
+
+# pubsublite parameters
+project_number = "hive-413217"
+cloud_region = "us-central1"
+zone_id = "c"
+reservation_id = "reservation"
+throughput_capacity = 4
+num_partitions = 2
+regional = True
+txn_topic_id = "transaction"
+inv_topic_id = "inventory"
