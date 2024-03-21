a2c36850012cb482e6d3a0e0e9e6451f561673b4
sl2902 <laxmsun@hotmail.com>
2024-03-22 01:24:06 +0530

    Add new operator to create dataproc cluster

--- airflow_pubsublite_bigquery/dags/airflow_publish_stream_to_bq.py
+++ airflow_pubsublite_bigquery/dags/airflow_publish_stream_to_bq.py
@@ -24,6 +24,7 @@ from airflow.providers.google.cloud.hooks.dataproc import (
     DataprocHook
 )
 from airflow.providers.google.cloud.operators.dataproc import (
+    ClusterGenerator,
     DataprocCreateClusterOperator,
     DataprocSubmitPySparkJobOperator,
     DataprocDeleteClusterOperator
@@ -45,14 +46,24 @@ import json
 from mock_data_scripts.generate_mock_stream_data import run_pipeline
 from config_data.gcp_config_parameters import *
 from config_data.pubsublite_config import *
+import logging
+from dotenv import load_dotenv
+
+load_dotenv()
+logger = logging.getLogger("airflow.task")
 
 # gcp_info = Variable.get("gcp_info", deserialize_json=True)
 # pubsub_info = Variable.get("pubsublite_info", deserialize_json=True)
 # cluster_configs = Variable.get("cluster_info", deserialize_json=True)
-bucket_folder = "generated_data"
+bucket_folder = os.getenv('mock_data_subfolder')
 dim_products = f"{bucket_folder}/dim_products.json"
 dim_stores = f"{bucket_folder}/dim_stores.json"
 parent_path = Path(__file__).resolve().parent
+# jars = [
+#     f"gs://{BUCKET_NAME}/jars/{filename}" for filename in os.listdir(f"{parent_path}/jars")
+#     if not ("bigquery" in filename)
+# ]
+
 jars = [f"gs://{BUCKET_NAME}/jars/pubsublite-spark-sql-streaming-1.0.0-with-dependencies.jar",
         f"gs://{BUCKET_NAME}/jars/google-cloud-pubsublite-1.9.0.jar"
 ]
@@ -60,9 +71,11 @@ jars = [f"gs://{BUCKET_NAME}/jars/pubsublite-spark-sql-streaming-1.0.0-with-depe
 pyspark_producer_main_path = f"gs://{BUCKET_NAME}/pyspark_scripts/pubsublite_pyspark_stream_producer.py"
 pyspark_consumer_main_path = f"gs://{BUCKET_NAME}/pyspark_scripts/pubsublite_pyspark_stream_consumer.py"
 config_files = [f"gs://{BUCKET_NAME}/config_data/"]
+# config_files = \
 # [
 #     f"gs://{BUCKET_NAME}/config_data/gcp_config_parameters.py",
 #     f"gs://{BUCKET_NAME}/config_data/pubsublite_config.py",
+#     f"gs://{BUCKET_NAME}/config_data/.env",
 # ]
 
 def read_cluster_config(**kwargs):
@@ -73,10 +86,11 @@ def read_cluster_config(**kwargs):
     except Exception as e:
         raise Exception(f"Dataproc config file missing")
     kwargs["task_instance"].xcom_push(key="cluster_configs", value=cluster_configs)
+    logger.info("value of kwargs", cluster_configs)
 
 
 def check_dataproc_cluster(**kwargs):
-    dataproc_hook = DataprocHook(gcp_conn_id='google_cloud_default')
+    dataproc_hook = DataprocHook(gcp_conn_id=os.getenv("GOOGLE_CONN_ID"))
     cluster_configs = kwargs['ti'].xcom_pull(task_ids='load_cluster_config', key='cluster_configs')
     try:
         cluster = dataproc_hook.get_cluster(project_id=PROJECT_ID, 
@@ -121,17 +135,6 @@ class UploadLocalFolderToGCS(BaseOperator):
                     blob = bucket.blob(f"{destination_filename}/{file}")
                     blob.upload_from_filename(os.path.join(self.src, file))
                     self.log.info(f"File {file} uploaded to {destination_filename}.")
-            # filenames = [file if file.startswith(self.prefix) else file for file in os.listdir(self.src) ]
-            # results = transfer_manager.upload_many_from_filenames(
-            #             self.bucket_name, filenames, source_directory=self.src, max_workers=self.workers
-            # )
-            # for name, result in zip(filenames, results):
-            #     # The results list is either `None` or an exception for each filename in
-            #     # the input list, in order.
-            #     if isinstance(result, Exception):
-            #         self.log.info("Failed to upload {} due to exception: {}".format(name, result))
-            #     else:
-            #         self.log.info("Uploaded {} to {}.".format(name, bucket.name))
         except NotFound:
             self.log.info(f"Bucket '{self.bucket_name}' not found.")
             raise NotFound(f"Bucket '{self.bucket_name}' not found.")
@@ -164,38 +167,38 @@ run_date = "{{ dag_run.conf['execution_date'] if dag_run and dag_run.conf and 'e
 #     bash_command = f"gsutil cp -r {config_file_path} gs://{BUCKET_NAME}/",
 #     dag=dag
 # )
-load_config_files = UploadLocalFolderToGCS(
-    task_id="load_config_files",
-    project_id=PROJECT_ID,
-    bucket_name=BUCKET_NAME,
-    src=config_file_path,
-    exclude="__"
-)
+# load_config_files = UploadLocalFolderToGCS(
+#     task_id="load_config_files",
+#     project_id=PROJECT_ID,
+#     bucket_name=BUCKET_NAME,
+#     src=config_file_path,
+#     exclude="__"
+# )
 
 # load_jar_files = BashOperator(
 #     task_id="load_jar_files",
 #     bash_command = f"gsutil cp -r {jar_file_path} gs://{BUCKET_NAME}/",
 #     dag=dag
 # )
-load_jar_files = UploadLocalFolderToGCS(
-    task_id="load_jar_files",
-    project_id=PROJECT_ID,
-    bucket_name=BUCKET_NAME,
-    src=jar_file_path
-)
+# load_jar_files = UploadLocalFolderToGCS(
+#     task_id="load_jar_files",
+#     project_id=PROJECT_ID,
+#     bucket_name=BUCKET_NAME,
+#     src=jar_file_path
+# )
 
 # load_pyspark_files = BashOperator(
 #     task_id="load_pyspark_files",
 #     bash_command = f"gsutil cp -r {pyspark_file_path} gs://{BUCKET_NAME}/pyspark_scripts/",
 #     dag=dag
 # )
-load_pyspark_files = UploadLocalFolderToGCS(
-    task_id="load_pyspark_files",
-    project_id=PROJECT_ID,
-    bucket_name=BUCKET_NAME,
-    src=pyspark_file_path,
-    exclude="__"
-)
+# load_pyspark_files = UploadLocalFolderToGCS(
+#     task_id="load_pyspark_files",
+#     project_id=PROJECT_ID,
+#     bucket_name=BUCKET_NAME,
+#     src=pyspark_file_path,
+#     exclude="__"
+# )
 
 
 generate_stream_data = PythonOperator(
@@ -230,14 +233,47 @@ one_success = DummyOperator(
         task_id='one_task_success',
         dag=dag,
         trigger_rule=TriggerRule.ONE_SUCCESS,
-    )
+)
 
+# init_action_uris = "{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['INITIALIZATION_ACTIONS']}}"
+init_action_uris = "gs://goog-dataproc-initialization-actions-{REGION}/python/pip-install.sh"
+init_action_uris = [init_action_uris.format(REGION=REGION)]
+# num_workers = "{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['CLUSTER_CONFIG']['worker_config']['num_instances']}}"
+# num_masters = "{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['CLUSTER_CONFIG']['master_config']['num_instances']}}"
+master_machine_type = "{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['CLUSTER_CONFIG']['master_config']['machine_type_uri']}}"
+master_disk_type = "{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['CLUSTER_CONFIG']['master_config']['disk_config']['boot_disk_type']}}"
+master_disk_size = "{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['CLUSTER_CONFIG']['master_config']['disk_config']['boot_disk_size_gb']}}"
+work_machine_type = num_workers = "{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['CLUSTER_CONFIG']['worker_config']['machine_type_uri']}}"
+worker_disk_type = "{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['CLUSTER_CONFIG']['worker_config']['disk_config']['boot_disk_type']}}"
+# throws error: cannot convert str to int. Type casting doesn't work. But when xcom jinja is passed directly like shown below, it works
+# worker_disk_size = "{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['CLUSTER_CONFIG']['worker_config']['disk_config']['boot_disk_size_gb']}}"
+image_version = "{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['CLUSTER_CONFIG']['software_config']['image_version']}}"
+
+cluster_configs = ClusterGenerator(
+    task_id="create_cluster",
+    cluster_name="{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['CLUSTER_NAME']}}",
+    project_id=PROJECT_ID,
+    region=REGION,
+    zone=f"{REGION}-{zone_id}",
+    storage_bucket=BUCKET_NAME,
+    metadata="{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['METADATA']}}",
+    init_actions_uris=init_action_uris,
+    num_workers="{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['CLUSTER_CONFIG']['worker_config']['num_instances']}}",
+    num_masters="{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['CLUSTER_CONFIG']['master_config']['num_instances']}}",
+    master_machine_type=master_machine_type,
+    work_machine_type=work_machine_type,
+    master_disk_size="{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['CLUSTER_CONFIG']['master_config']['disk_config']['boot_disk_size_gb']}}",
+    worker_disk_size="{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['CLUSTER_CONFIG']['worker_config']['disk_config']['boot_disk_size_gb']}}",
+    image_version=image_version
+).make()
+
+# cluster_configs = "{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['CLUSTER_CONFIG']}}"
 create_cluster = DataprocCreateClusterOperator(
     task_id="create_cluster",
     cluster_name="{{task_instance.xcom_pull(task_ids='load_cluster_config', key='cluster_configs')['CLUSTER_NAME']}}",
     project_id=PROJECT_ID,
     region=REGION,
-    cluster_config='{{task_instance.xcom_pull(task_ids="load_cluster_config", key="cluster_configs")["CLUSTER_CONFIG"]}}',
+    cluster_config=cluster_configs,
     dag=dag
 )
 
@@ -278,7 +314,7 @@ delete_cluster = DataprocDeleteClusterOperator(
     dag=dag
 )
 
-[load_config_files, load_pyspark_files, load_jar_files] >> generate_stream_data >> load_cluster_config >> check_cluster_task >> [cluster_running, create_cluster]
+generate_stream_data >> load_cluster_config >> check_cluster_task >> [cluster_running, create_cluster]
 [cluster_running, create_cluster] >> one_success >> [consumer_job, producer_job]
 [consumer_job, producer_job] >> all_success >> delete_cluster
 
