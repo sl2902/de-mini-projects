42b8631bc6a93e9a25083139f2ca2ab19f9c1609
sl2902 <laxmsun@hotmail.com>
2024-03-22 01:45:53 +0530

    Add new module to parse env variables

--- airflow_pubsublite_bigquery/dags/config_data/pubsublite_config.py
+++ airflow_pubsublite_bigquery/dags/config_data/pubsublite_config.py
@@ -1,6 +1,11 @@
-project_number = "hive-413217"
-cloud_region = "us-central1"
-zone_id = "c"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+project_number = os.getenv('project_id')
+cloud_region = os.getenv('region')
+zone_id = os.getenv('zone_id')
 reservation_id = "reservation_2"
 throughput_capacity = 4
 num_partitions = 2
